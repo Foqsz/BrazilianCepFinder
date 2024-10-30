3b54aa19683798bed2f4f692a0fc0f1f72f38459
Foqsz <contatovictorvinicius05@gmail.com>
2024-10-30 10:49:16 -0300

    Aprimoramento do Service. Retirando nullable e filtrando objeto.

--- Api-Cep-Correios/Services/BuscaCepService.cs
+++ Api-Cep-Correios/Services/BuscaCepService.cs
@@ -1,4 +1,7 @@
-﻿using Api_Cep_Correios.Services.Interfaces;
+﻿using Api_Cep_Correios.Model;
+using Api_Cep_Correios.Services.Interfaces;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
 
 namespace Api_Cep_Correios.Services;
 
@@ -16,13 +19,41 @@ public class BuscaCepService : IBuscaCepService
         string baseUrl = "https://viacep.com.br/ws/";
         string fullUrl = $"{baseUrl}{cep}/json/";
 
-        var response = await _httpClient.GetAsync(fullUrl);
-
-        if (response.IsSuccessStatusCode)
+        try
         {
-            var content = await response.Content.ReadAsStringAsync();
-            return content; 
+            var response = await _httpClient.GetAsync(fullUrl);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync(); //converte em string
+
+                var cepJsonObject = JsonConvert.DeserializeObject<CepModel>(content);
+
+                cepJsonObject.Complemento = cepJsonObject.Complemento.IfNullOrEmpty("Sem complemento definido");
+                cepJsonObject.Unidade = cepJsonObject.Unidade.IfNullOrEmpty("Sem unidade definida");
+                cepJsonObject.Gia = cepJsonObject.Gia.IfNullOrEmpty("Sem GIA definida");
+
+                string cepStringFiltred = JsonConvert.SerializeObject(cepJsonObject, Formatting.Indented);
+
+                return cepStringFiltred;
+            }
+            else
+            {
+                return $"Nenhuma localização para o CEP {cep}";
+            }
         }
-        return null;
+        catch (Exception ex)
+        {
+            return $"Erro! {ex.Message}";
+        }
     }
 }
+
+public static class StringExtensions
+{
+    public static string IfNullOrEmpty(this string value, string defaultValue)
+    {
+        return string.IsNullOrEmpty(value) ? defaultValue : value;
+    }
+}
+
