4a24d88a05d10620216c80ce70ee246a5a1a0f71
Foqsz <contatovictorvinicius05@gmail.com>
2024-11-02 23:49:16 -0300

    refactor: adicioned documentation

--- Api-Cep-Correios/Program.cs
+++ Api-Cep-Correios/Program.cs
@@ -2,6 +2,7 @@ using Api_Cep_Correios.Controllers;
 using Api_Cep_Correios.Services;
 using Api_Cep_Correios.Services.Interfaces;
 using Api_Cep_Correios.Services.Refit;
+using Microsoft.OpenApi.Models;
 using Refit;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,8 +12,27 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
- 
+builder.Services.AddSwaggerGen(c =>
+
+        c.SwaggerDoc("v1", new OpenApiInfo()
+        {
+            Version = "v1",
+            Title = "LibraryWebApi",
+            Description = "Api de Gerenciamento de Biblioteca",
+            TermsOfService = new Uri("https://foqsz.github.io/"),
+            Contact = new OpenApiContact
+            {
+                Name = "Victor Vinicius",
+                Email = "contatovictorvinicius05@gmail.com",
+                Url = new Uri("https://foqsz.github.io/"),
+            },
+            License = new OpenApiLicense
+            {
+                Name = "Usar sobre LICX",
+                Url = new Uri("https://foqsz.github.io/"),
+            }
+}));
+
 builder.Services.AddScoped<IBuscaCepIntegracao, BuscaCepIntegracacao>();
 
 builder.Services.AddRefitClient<IBuscaCepIntegracaoRefit>().ConfigureHttpClient(c =>
